666461b9b762f734030f3cf8671af5b3b1634b12
Francis <fnkorley@gmail.com>
2026-01-02 03:38:55 +0000

    Prepare project for PythonAnywhere deployment

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from .views import UserListCreateView, UserDetailView, LoginView
 urlpatterns = [
     path('', UserListCreateView.as_view()),
     path('<int:pk>/', UserDetailView.as_view()),
-    path('login/', LoginView.as_view()),  # Login
+    path('login/', LoginView.as_view(), name='login'),
 ]
